b3a6a1d84d2d57955464182e651368ce31279261
CHRISTENSEN Pierre <pchristensen@centrepatronal.ch>
2020-10-12 07:19:28 +0200

    Bug fix on fillable fields

--- app/Database/Games.php
+++ app/Database/Games.php
@@ -11,7 +11,7 @@ class Games extends Model {
    public $timestamps = true;
    protected $table = 'games';
    protected $primaryKey = 'id';
-   protected $fillable = ['games_playlist,games_currenttrackindex,games_currenttrack_starttime'];
+   protected $fillable = ['games_playlist', 'games_currenttrackindex', 'games_currenttrack_starttime'];
   
 
    /**
