c41c50b42ba46d2f227eb092d79204199d9fd177
Pierre Christensen <pierre.christensen@gmail.com>
2020-11-15 18:42:08 +0100

    update the Docker part

--- Docker/startup.sh
+++ Docker/startup.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #apt-get install -y composer git libapache2-mod-php php-mbstring
-git clone https://github.com/pitch7900/MusicMigrator.git
-rm -rf ./MusicMigrator/vendor
-mv MusicMigrator musicmigrator
-cd musicmigrator
+git clone https://github.com/pitch7900/BlindTest.git
+rm -rf ./BlindTest/vendor
+mv BlindTest BlindTest
+cd BlindTest
 composer install
 cd ..
-tar -czf musicmigrator.tar.gz musicmigrator
+tar -czf BlindTest.tar.gz BlindTest
 docker build .
 echo "Start now the container with : docker-compose up" 
